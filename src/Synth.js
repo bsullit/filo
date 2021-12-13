585f69725f4fcf8b3f0a2f6e3af69de476a53d9e
bsullit <brian.sullivan.ic@gmail.com>
2021-12-13 12:31:53 -0500

    reorganizing react boilerplate

---
+++ src/Synth.js
@@ -0,0 +1,14 @@
+import * as Tone from 'tone'
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector, connect } from 'react-redux';
+
+
+const main = () => {
+return (
+  <p>synth comp</p>
+)
+}
+
+
+
+export default connect(null)(main)
